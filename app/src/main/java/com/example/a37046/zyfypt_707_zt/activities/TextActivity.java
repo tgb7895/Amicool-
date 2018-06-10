b02b4e7fbec85b6bce38eefc782889e27e1bcaa6
tgb7895 <370465054@qq.com>
2018-06-10 15:29:12 +0800

    提示框写了一般 功能基本全部完成（暂时无bug）

--- app/src/main/java/com/example/a37046/zyfypt_707_zt/activities/TextActivity.java
+++ app/src/main/java/com/example/a37046/zyfypt_707_zt/activities/TextActivity.java
@@ -1,8 +1,13 @@
 package com.example.a37046.zyfypt_707_zt.activities;
 
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.example.a37046.zyfypt_707_zt.R;
@@ -14,7 +19,32 @@ public class TextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+        Button button=findViewById(R.id.button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog2 = new AlertDialog.Builder(v.getContext())
+                        .setTitle("这是标题")
+                        .setMessage("有多个按钮")
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {//添加"Yes"按钮
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(TextActivity.this, "这是确定按钮", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {//添加取消
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(TextActivity.this, "这是取消按钮", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .create();
+                alertDialog2.show();
+            }
+        });
     }
 
 
